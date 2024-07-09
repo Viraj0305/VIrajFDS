421df551e4b0939705cc2326249f8a1a53dfb6e3
Viraj0305 <kapseviraj0013@gmail.com>
2024-07-09 23:09:11 +0530

    Add files via upload

---
+++ odd_even.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number"<<endl;
+    cin>>n;
+
+    if(n%2==0)
+    {
+        cout<<"Even number"<<endl;
+    }
+
+    else{
+        cout<<"Odd number"<<endl;
+    }
+}
